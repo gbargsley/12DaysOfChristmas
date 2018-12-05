2126ba4f32db6434de5efc0b82e27f642eff73f7
Elevated User - Garry Bargsley <e_gbargsley@exclaim-crp.com>
2018-12-05 09:21:08 -0600

    Updated first day

---
+++ FirstDay-FindDbaCommand.ps1
@@ -0,0 +1,6 @@
+
+## Discover what dbatools can do for you
+Find-DbaCommand -Pattern CPU
+
+## Advanced find and get-help
+Find-DbaCommand -Pattern CPU | Select-Object Tags | Sort-Object Tags -Unique
