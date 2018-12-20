a01ed747ad4c2c2b7eec227ea9aad824fb8e3c9e
Elevated User - Garry Bargsley <e_gbargsley@exclaim-crp.com>
2018-12-20 09:14:05 -0600

    Day 12 change

--- TwelfthDay-Start-DbaMigration.ps1
+++ TwelfthDay-Start-DbaMigration.ps1
@@ -1,2 +1,3 @@
 # Lets migrate 2017 to 2019
-Start-DbaMigration -
+Start-DbaMigration -Source localhost\sql2016 -Destination localhost\sql2019 -BackupRestore `
+     -SharedPath \\LXDW17181\C$\Temp\Backups -SetSourceReadOnly
